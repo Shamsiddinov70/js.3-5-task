3656254386ca0fc0e94f0a3289a690d812390155
Shamsiddinov70 <abbosshamsiddin@gmail.com>
2022-04-30 12:52:04 +0500

    add DOM changes

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ var sum = dollar + euro;
 
 if (expense >= sum) {
     console.log('Oq yo’l, Alisher!');
+    document.getElementById("demo1").innerHTML = "Oq yo'l jigar";
 }
 
 else if (expense < sum) {
     console.log('Alisher, ozgina sabr qilish kerak bo’lar ekan.');
+    document.getElementById("demo2").innerHTML = "Pul yo'q ekanu jigar sayohatga balo bormi?";
 }
 
 else {
     console.log('error')
 }
 
-console.log(`Umumiy ketadigan xarajatlar: ${sum}`)
+console.log(`Umumiy ketadigan xarajatlar: ${sum} so\'m`)
